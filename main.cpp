7e244e128d9f29d02c08b7d6a5efc85c020c8c3e
Danny Kerr <97600638+itsDanz0r@users.noreply.github.com>
2022-01-28 15:20:07 +1030

    Add resolution and center attributes
    Mucking around to get a handle on syntax

--- main.cpp
+++ main.cpp
@@ -9,12 +9,24 @@ class Tracer;
 class Roulette{
 public:
     int frame = 0;
+    double rotations = 10;
+    int resolution[2];
+    float center[2];
+    Roulette(){
+        resolution[0] = 3840;
+        resolution[1] = 2160;
+        center[0] = resolution[0] / 2;
+        center[1] = resolution[1] / 2;
+    }
 };
-
 
 int main() {
     Roulette my_roulette = Roulette();
     cout << my_roulette.frame << endl;
     my_roulette.frame += 1;
-    cout << my_roulette.frame;
+    cout << my_roulette.frame << endl;
+    for (int i=0; i<2; i++) {
+        cout << my_roulette.resolution[i] << ' ' << my_roulette.center[i] << endl;
+    }
 }
+
